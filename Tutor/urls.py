f206d09ee56a076e5be18fe73d5dad7339f3115b
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-12-07 19:26:12 +0300

    added urls

---
+++ Tutor/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url('^$',views.index,name = 'index'),
+    url(r'^tutor/(\d+)',views.tutor,name ='tutor'),
+    url(r'^location/',views.location,name ='location'),
+    url(r'^name/',views.name,name ='name'),
+    url(r'^experience/',views.experience,name ='experience')
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
