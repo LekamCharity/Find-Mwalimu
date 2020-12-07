d419e007f3a4120f7dee3082386600936b8ac690
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-12-07 20:22:43 +0300

    added a new template

--- Tutor/views.py
+++ Tutor/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
-
 def index(request):
     tutors = Tutor.display_tutor()
-    return render(request, "template", {"tutors":tutors}) 
+    return render(request, "index.html", {"tutors":tutors}) 
 
 
 def tutor(request,tutor_id):
@@ -13,7 +12,7 @@ def tutor(request,tutor_id):
         tutor = Tutor.objects.filter(id = tutor_id)
     except DoesNotExist:
         raise Http404()
-    return render(request,"template", {"tutor":tutor})
+    return render(request,"mwalimu.html", {"tutor":tutor})
 
 
 def location(request):
@@ -21,11 +20,11 @@ def location(request):
         search_term = request.GET.get("tutor")
         searched_tutors = Tutor.find_tutor_location(search_term)
         message = f"{search_term}"
-        return render(request, 'template',{"message":message,"tutors": searched_tutors})
+        return render(request, 'location.html',{"message":message,"tutors": searched_tutors})
     else:
         message = "Project does not exist"
-        return render(request, 'template',{"message":message})
-    return render(request, "template")
+        return render(request, 'location.html',{"message":message})
+    return render(request, "location.html")
 
 
 def name(request):
@@ -33,19 +32,19 @@ def name(request):
         search_term = request.GET.get("tutor")
         searched_tutors = Tutor.find_tutor_name(search_term)
         message = f"{search_term}"
-        return render(request, 'template',{"message":message,"tutors": searched_tutors})
+        return render(request, 'mwalimus_name.html',{"message":message,"tutors": searched_tutors})
     else:
         message = "Project does not exist"
-        return render(request, 'template',{"message":message})
-    return render(request, "template")
+        return render(request, 'mwalimus_name.html',{"message":message})
+    return render(request, "mwalimus_name.html")
 
 def experience(request):
     if 'tutor' in request.GET and request.GET["tutor"]:
         search_term = request.GET.get("tutor")
         searched_tutors = Tutor.find_programming_experience(search_term)
         message = f"{search_term}"
-        return render(request, 'template',{"message":message,"tutors": searched_tutors})
+        return render(request, 'mwalimus_experience.html',{"message":message,"tutors": searched_tutors})
     else:
         message = "Project does not exist"
-        return render(request, 'template',{"message":message})
-    return render(request, "template")
+        return render(request, 'mwalimus_experience.html',{"message":message})
+    return render(request, "mwalimus_experience.html")
