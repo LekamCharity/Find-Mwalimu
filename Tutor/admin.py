183656b396774d6293cb39c6e92ce7de7ff9c9ba
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-12-07 19:29:17 +0300

    added templates

--- Tutor/admin.py
+++ Tutor/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
+class TutorAdmin(admin.ModelAdmin):
+    filter_horizontal =('languages',)
 
 # Register your models here.
+
+admin.site.register(Tutor, TutorAdmin)
+admin.site.register(Experience)
+admin.site.register(Language)
